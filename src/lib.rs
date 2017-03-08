42dc8d94fcdc7765669a17b5f34e1a122861df23
nvonbulow <nick@nvonbulow.com>
2017-03-08 16:57:58 -0500

    Add interrupt framework

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,35 @@
 #![feature(lang_items)]
 #![feature(const_fn)]
 #![feature(unique)]
+#![feature(asm)]
 
 #![no_std]
 
 extern crate rlibc; //Memory functions
 extern crate volatile;
 extern crate spin;
-
+extern crate bit_field;
+#[macro_use]
+extern crate lazy_static;
 #[macro_use]
 extern crate x86;
 
 #[macro_use]
 mod vga;
+mod interrupts;
 
 #[no_mangle]
 pub extern fn rust_main() -> ! {
+    interrupts::init();
     vga::text::clear_buffer();
-
-    println!("Hello Rust");
-    print!("Hello RUST");
+    divide_by_zero();
     loop {}
+}
+
+fn divide_by_zero() {
+    unsafe {
+        asm!("mov dx, 0; div dx" ::: "ax", "dx" : "volatile", "intel")
+    }
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
